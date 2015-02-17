0d464c18f805ecd56998e2f51bf803a061b1a3b8
Sherrie Jones <sj24kt@comcast.net>
2015-02-17 15:31:37 -0700

    first commit

---
+++ Ribbit/CameraViewController.h
@@ -0,0 +1,13 @@
+//
+//  CameraViewController.h
+//  Ribbit
+//
+//  Created by Sherrie Jones on 2/16/15.
+//  Copyright (c) 2015 Sherrie Jones. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CameraViewController : UITableViewController
+
+@end
---
+++ Ribbit/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  Ribbit
+//
+//  Created by Sherrie Jones on 2/14/15.
+//  Copyright (c) 2015 Sherrie Jones. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
